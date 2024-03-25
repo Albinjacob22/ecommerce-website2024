5d49bc78150f78dc8bb8c0ca866d24151efeb565
Albinjacob22 <albinphilip20@gmail.com>
2024-03-25 10:43:50 +0530

    admin home page css updated

--- src/app/page.js
+++ src/app/page.js
@@ -160,7 +160,7 @@ export default function Home() {
                     onClick={() => router.push("/product/listing/men")}
                     className="mt-1.5 inline-block bg-black px-5 py-3 text-xs font-medium uppercase tracking-wide text-white"
                   >
-                    Shop Now
+                    
                   </button>
                 </div>
               </div>
